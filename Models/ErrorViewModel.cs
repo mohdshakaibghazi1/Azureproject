e2cc16c746dc4bf03fba85826af9129f90901705
Mohd Shakaib Ghazi <mohd.shakaibghazi@mphasis.com>
2023-09-20 12:16:24 +0530

    frst

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WeaApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
